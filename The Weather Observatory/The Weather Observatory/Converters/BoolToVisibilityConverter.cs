c95b6d72db816fe67a8d8ef1b03f7b88000385ea
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-10-10 18:24:41 +0200

    create Activity Indicator and BoolToVisibilityConverter to display Loader when content loading

---
+++ The Weather Observatory/The Weather Observatory/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace The_Weather_Observatory.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? true : false;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool visibilityValue)
+            {
+                return visibilityValue;
+            }
+            return false;
+        }
+    }
+}
